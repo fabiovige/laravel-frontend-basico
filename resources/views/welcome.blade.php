f8befbd2307b55e13c9c7b017377aa64a5e2512a
Fabio Martins <fabiovige@gmail.com>
2024-06-02 09:54:35 -0300

    estudando tailwindcss

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="min-h-screen w-full flex items-center">
+    <div class="w-full bg-red-100 p-4">
+        Hello world!
+    </div>
+</body>
+</html>
